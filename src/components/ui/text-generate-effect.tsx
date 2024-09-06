73b0e5adeea2ea067793cb1911aa83d440c7b8d6
RuffledZest <vibhanshalok@gmail.com>
2024-09-07 03:58:54 +0530

    updated the landing page, also implemented the dark/light mode

--- src/components/ui/text-generate-effect.tsx
+++ src/components/ui/text-generate-effect.tsx
@@ -38,7 +38,7 @@ export const TextGenerateEffect = ({
           return (
             <motion.span
               key={word + idx}
-              className="body-1 max-w-3xl mx-auto mb-2 text-n-2 lg:mb-2 opacity-0"
+              className="body-1 max-w-3xl mx-auto mb-2 text-slate-600 dark:text-slate-500 lg:mb-2 opacity-0"
               style={{
                 filter: filter ? "blur(10px)" : "none",
               }}
